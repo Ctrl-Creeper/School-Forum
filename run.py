b94601b07ec39e31b60a4477ef40c76723cbde1d
Void_DRS <robomaster004@gmail.com>
2025-09-23 08:20:32 +0800

    更改冲突的端口

--- run.py
+++ run.py
@@ -14,4 +14,4 @@ app.register_blueprint(forget_bp)
 app.register_blueprint(health_bp)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=1145, debug=True)
