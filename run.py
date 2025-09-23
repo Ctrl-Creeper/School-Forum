7d0c33a3e3e4f0b5ad02832328d599498e898ad2
xiaokai-lyk <105791771+xiaokai-lyk@users.noreply.github.com>
2025-09-23 09:22:54 +0800

    fix: 从config里读取端口

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ from app.routes.login import login_bp
 from app.routes.logout import logout_bp
 from app.routes.forget import forget_bp
 from app.routes.health import health_bp
+from app.utils.config import config
 
 # 注册所有 Blueprint
 app.register_blueprint(register_bp)
@@ -13,5 +14,6 @@ app.register_blueprint(logout_bp)
 app.register_blueprint(forget_bp)
 app.register_blueprint(health_bp)
 
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=1145, debug=True)
+    app.run(host="0.0.0.0", port=config['PORT'], debug=True)
